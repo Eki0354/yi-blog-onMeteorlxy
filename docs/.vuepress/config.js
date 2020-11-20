4f6476977f6f0d813ba6c0146f8b0c585fa9ac9d
Eki <ichinoseeki@outlook.com>
2020-11-20 15:14:47 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,115 @@
+const path = require('path')
+
+module.exports = {
+  title: '',
+
+  description: 'Eki\'s Blog',
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
+
+  evergreen: true,
+
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-132770851-2',
+    }],
+  ],
+
+  chainWebpack: (config, isServer) => {
+    if (isServer === false) {
+      config.optimization.splitChunks({
+        maxInitialRequests: 5,
+        cacheGroups: {
+          vue: {
+            test: /[\\/]node_modules[\\/](vue|vue-router|vssue)[\\/]/,
+            name: 'vendor.vue',
+            chunks: 'all',
+          },
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            name: 'vendor.commons',
+            chunks: 'all',
+          },
+        },
+      })
+    }
+  },
+
+  theme: path.resolve(__dirname, '../../lib'),
+
+  themeConfig: {
+    lang: 'zh-CN',
+
+    personalInfo: {
+      nickname: 'Eki',
+      description: '腹坚亦博',
+      email: 'ichinoseeki@outlook.com',
+      location: 'Chengdu, China',
+      organization: 'Nil',
+
+      avatar: 'https://www.meteorlxy.cn/assets/img/avatar.jpg',
+
+      sns: {
+        github: {
+          account: 'Eki0354',
+          link: 'https://github.com/Eki0354',
+        },
+      },
+    },
+
+    header: {
+      background: {
+        // url: '/assets/img/header-image-01.jpg',
+        useGeo: true,
+      },
+      showTitle: true,
+    },
+
+    footer: {
+      poweredBy: true,
+      poweredByTheme: true,
+      custom: 'Copyright 2018-present <a href="https://github.com/meteorlxy" target="_blank">meteorlxy</a> | MIT License',
+    },
+
+    infoCard: {
+      headerBackground: {
+        // url: '/assets/img/header-image-01.jpg',
+        useGeo: true,
+      },
+    },
+
+    lastUpdated: true,
+
+    nav: [
+      { text: '主页', link: '/', exact: true },
+      { text: '文章', link: '/posts/', exact: false },
+      // { text: 'Custom Pages', link: '/custom-pages/', exact: false },
+      { text: 'Github', link: 'https://github.com/Eki0354' },
+    ],
+
+    // Enable smooth scrolling or not
+    smoothScroll: false,
+
+    // Configs for vuepress-plugin-zooming
+    zooming: {
+      // @see https://vuepress.github.io/en/plugins/zooming
+    },
+
+    comments: {
+      owner: 'meteorlxy',
+      repo: 'vuepress-theme-meteorlxy',
+      clientId: 'cbda894952ba70c00666',
+      clientSecret: '1ade785ca693bf3092be5e5338720d5ee43672b7',
+      autoCreateIssue: false,
+    },
+
+    pagination: {
+      perPage: 10,
+    },
+  },
+}
---
+++ docs/_posts/2020-11-20-element-ui-table-no-data-text.md
@@ -0,0 +1,14 @@
+---
+category: 坑
+tags:
+  - element-ui
+  - 表格
+date: 2020-11-20
+title: ElTabel组件空数据显示文本固定居中问题
+---
+
+`暂无数据`
+
+<!-- more -->
+
+## 测试内容
